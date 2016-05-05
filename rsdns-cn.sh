c92237ec8997919989520e58dbd61cc8ca8e8a2d
Nick <linickx@gmail.com>
2016-05-05 12:07:49 +0100

    rsdns-cn add -J

--- rsdns-cn.sh
+++ rsdns-cn.sh
@@ -26,6 +26,7 @@ function usage () {
 	printf "\t-k Use London/UK Servers.\n"
 	printf "\t-x Delete record.\n"
 	printf "\t-U Update existing record.\n"
+	printf "\t-J Output in JSON (raw RS API data)\n"
 	printf "\t-h Show this.\n"
 	printf "\n"
 }
@@ -96,7 +97,7 @@ function words () {
 }
 
 #Get options from the command line.
-while getopts "u:a:c:n:d:r:t::hkqxUw" option
+while getopts "u:a:c:n:d:r:t::hkqxUwJ" option
 do
 	case $option in
 		u	) RSUSER=$OPTARG ;;
@@ -112,6 +113,7 @@ do
 		x	) DEL=1 ;;
 		U	) UPDATE=1 ;;
 		w	) words;exit 0 ;;
+		J	) RSJSON=1 ;;
 	esac
 done
 
