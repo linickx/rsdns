3b7369eb4c4f2b8fd3900a946d6ca5c018b9e6d8
Nick <linickx@gmail.com>
2016-05-06 14:39:40 +0100

    put a update ip in the echo/log

--- tests/a.sh
+++ tests/a.sh
@@ -2,5 +2,5 @@
 
 echo "Create a record: arecord.$TESTDOMAIN"
 $PWD/rsdns a -d $TESTDOMAIN -n arecord.$TESTDOMAIN -i 1.2.3.4
-echo "Update a record: arecord.$TESTDOMAIN"
+echo "Update a record: arecord.$TESTDOMAIN - 1.2.3.3"
 $PWD/rsdns a -d $TESTDOMAIN -n arecord.$TESTDOMAIN -i 1.2.3.3 -U
