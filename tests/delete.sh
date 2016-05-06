5c1cfed0ac897ba54d3ef99fc6c01fca4ea3ec45
Nick <linickx@gmail.com>
2016-05-06 14:35:27 +0100

    fix delete

--- tests/delete.sh
+++ tests/delete.sh
@@ -2,8 +2,8 @@
 
 # A
 echo "Delete arecord.$TESTDOMAIN"
-./rdns a -x -d $TESTDOMAIN -n arecord.$TESTDOMAIN
+$PWD/rdns a -x -d $TESTDOMAIN -n arecord.$TESTDOMAIN
 
 # Domain (Has to be LAST)
 echo "Delete domain $TESTDOMAIN"
-./rsdns domain -d $TESTDOMAIN -x
+$PWD/rsdns domain -d $TESTDOMAIN -x
